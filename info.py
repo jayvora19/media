4e7a3bf7a1da41a446da7aa47848761a9cb78e01
jayvora19 <75425454+jayvora19@users.noreply.github.com>
2023-05-17 09:30:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,12 +4,12 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
+SESSION = environ.get('BQGZ4aYAb7SbzAcwkRxG_ICO_fcDZMR6BIO3vIKApsfqKWJT-2-_oaEVmxn1CXvx-hi9s14_ennY0WMclgKkTQaFT-aQqN4HSt5BdRQJ6B99Er0pmAR5iij29Axu0RGVP8Zf3YqX7_hHFxYKkPm0MwpEjGvRNQT8ZqQssm2mcMF1O2sM5CI3YA7JgkYuOJeDKtNiyOXr0Y_kKp-tQaiUYkFq-D1jrHoNzHS2oMBU77b10gFiZHIBDD0kd4x9-17HB4OG-33qjcMMUPbFv-dkpYDueIX8HerB0v2yb6OItXiXq7rAuJX_mj5-yGt_eOSq275A4iMUDH1DzeM4WAAaiB4QZAIXvwAAAAFnifBOAA', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+API_ID = int(environ['26861990'])
+API_HASH = environ['0592761ae3a24dcf709d85ab87bc12b9']
+BOT_TOKEN = environ['0592761ae3a24dcf709d85ab87bc12b9']
+USERBOT_STRING_SESSION = environ.get('BQGZ4aYAb7SbzAcwkRxG_ICO_fcDZMR6BIO3vIKApsfqKWJT-2-_oaEVmxn1CXvx-hi9s14_ennY0WMclgKkTQaFT-aQqN4HSt5BdRQJ6B99Er0pmAR5iij29Axu0RGVP8Zf3YqX7_hHFxYKkPm0MwpEjGvRNQT8ZqQssm2mcMF1O2sM5CI3YA7JgkYuOJeDKtNiyOXr0Y_kKp-tQaiUYkFq-D1jrHoNzHS2oMBU77b10gFiZHIBDD0kd4x9-17HB4OG-33qjcMMUPbFv-dkpYDueIX8HerB0v2yb6OItXiXq7rAuJX_mj5-yGt_eOSq275A4iMUDH1DzeM4WAAaiB4QZAIXvwAAAAFnifBOAA')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -24,9 +24,9 @@ auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ['mongodb+srv://jayvora:1850@cluster.t5ql7o3.mongodb.net/?retryWrites=true&w=majority']
+DATABASE_NAME = environ['Cluster']
+COLLECTION_NAME = environ.get( 'Telegram_files')
 
 # Messages
 default_start_msg = """
