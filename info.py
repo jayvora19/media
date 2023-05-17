25c3737cc323047da0771d5f30dc69badcdb8948
jayvora19 <75425454+jayvora19@users.noreply.github.com>
2023-05-17 09:33:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,11 +16,11 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['6032060494'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['1001847058383'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('6032060494', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('-1001847058383')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
